b9cae3ed780949665286e15f7266e4be07923b1e
jmancuso42 <josephmancuso145@gmail.com>
2024-01-10 14:08:14 -0500

    need to break this down further, but making a save point before i break stuff

--- migration_end_dates/end_dates.py
+++ migration_end_dates/end_dates.py
@@ -1,3 +1,4 @@
+import logging
 import time
 import datetime
 import os
@@ -9,6 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait as wait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select  import Select
 from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.common.exceptions import (
     NoSuchElementException,
     WebDriverException,
@@ -20,9 +22,32 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.options import Options as EdgeOptions  # noqa
 from selenium.webdriver import EdgeService as EdgeService  # noqa
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
-class JoeDriver(webdriver.Edge, EdgeOptions=None, EdgeService=None):
-    def __init__(self, *args, **kwargs):
+
+#todo: add a way to compare the data from the two screens
+#todo: verify that it works up to the dropdown point
+#add a way, maybe regex, to select the role, and then the following value for the role
+#todo: add a wway to collect what's been done for an audit
+#todo: add an entry logic
+#todo: come up with corner cases
+#todo: add a way to check for the end date, and if it's not there, add it
+#todo, maybe add a click path for each program, or a way to input the program, or just have it wait till i select one
+
+
+validation_structure = []
+contacts_dict = {}  # checked on the info screen
+check_dict = {}  # checked on the history screen
+role_list = ['Dentist', 'Psychiatrist', 'Physician', 'Surgery', 'Therapist'] #maybe make this a dictionary with the role as the key and the value as the index, or regex the roles
+
+
+class JoeDriver(webdriver.Edge):
+    '''This class is a wrapper for the selenium webdriver.Edge class. It adds some additional functionality to the webdriver.Edge class.'''
+    #could probably break this up a bit, but IT'S FIIIINE
+    def __init__(self, wait, dropdown_index=None, EdgeOptions=None, EdgeService=None,
+                  expected_conditions=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._default_implicit_wait = 10
         self._default_page_load_wait = 10
@@ -31,7 +56,10 @@ class JoeDriver(webdriver.Edge, EdgeOptions=None, EdgeService=None):
         self.driver_path = 'msedgedriver.exe'
         self.options = Options()
         self.service = Service(self.driver_path)
+        self._username = os.environ.get('FOOTHOLD_USERNAME')
+        self._password = os.environ.get('FOOTHOLD_PASSWORD')
 
+      
     # Getter and setter for driver_path
     def get_driver_path(self):
         return self.driver_path
@@ -55,78 +83,119 @@ class JoeDriver(webdriver.Edge, EdgeOptions=None, EdgeService=None):
 
     def sign_in(self):
         # Find the username and password input fields
-        username_input = wait(self, 10).until(
-            EC.element_to_be_clickable((By.ID, 'handle'))
-        )
-        password_input = wait(self, 10).until(
-            EC.element_to_be_clickable((By.ID, 'password'))
-        )
 
-        # Clear the input fields (optional)
-        username_input.clear()
-        password_input.clear()
+        if EC.presence_of_element_located((By.ID, 'handle')):
+            print("found the username input")
+            username_input = wait(self, 10).until(
+                EC.element_to_be_clickable((By.ID, 'handle'))
+            )
+            password_input = wait(self, 10).until(
+                EC.element_to_be_clickable((By.ID, 'password'))
+            )
 
-        # Enter the username and password
-        username_input.send_keys(self._username)
-        password_input.send_keys(self._password)
+            # Clear the input fields (optional)
+            username_input.clear()
+            password_input.clear()
 
-        # Find and click the sign in button
-        sign_in_button = wait(self, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit'][name='submitbtn']"))
-        )
-        sign_in_button.click()
+            # Enter the username and password
+            username_input.send_keys(self._username)
+            password_input.send_keys(self._password)
 
-    def dropdown_select(self, dropdown_index=None, dropdown_locatorza=None, dropdown_length=None):
-        dropdown = Select(self.EC.find_element((By.ID, 'clientid_INT')))
-        if dropdown_index is not None:
-            dropdown.select_by_index(dropdown_index)
-            dropdown_name = dropdown.options[dropdown_index].text
+            # Find and click the sign in button
+            sign_in_button = wait(self, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit'][name='submitbtn']"))
+            )
+            sign_in_button.click()
         else:
-            dropdown.select_by_index(0)
-            dropdown_name = dropdown.options[0].text
-        return dropdown_name
+            pass
+    
+    def get_dropdown_length(self, dropdown_locator=None):
+        dropdown = self.find_element(By.ID, 'clientid_INT')
+        select = Select(dropdown)
+        dropdown_length = len(select.options)
+        return dropdown_length
 
     def submit_button_click(self, dropdown_index=None):
         first_button = self.find_element(By.CSS_SELECTOR, "input[type='submit']")
         first_button.click()
 
     def config_driver_default_instance_(self):
+        #configures for either default browser or exsisting
         pass
 
-    def compare_data(self):
+    def find_duplicate(self):
         pass
-
-    def open_browser_to_site(self):
+    def compare_duplicates(self):
+        pass
+    def end_record(self):
+        pass
+    
+    def dropdown_select(self, dropdown_index=None, dropdown_locator=None, dropdown_length=None, validation_structure=None): 
+        '''Selects a dropdown option by index and adds the name to a data structure for validation later'''
         try:
-            self.get('https://depaul.footholdtechnology.com')
-            if EC.presence_of_element_located((By.ID, 'handle')):
-                self.sign_in()
-            if EC.presence_of_element_located((By.ID, 'auth-code')):
-                print("waiting for 2fa")
-                wait(self, 60).until_not(EC.presence_of_element_located((By.ID, 'auth-code')))
-            self.get('https://depaul.footholdtechnology.com/awards/charts/medical/support_services_contacts')
-            wait(self, 45).until(method=EC.presence_of_element_located((By.ID, 'clientid_INT')))
-            self.dropdown_select()
-            self.submit_button_click()
-
+            dropdown = self.find_element(By.ID, 'clientid_INT')
+            wait(self,49).until.EC.presence_of_element_located(By.ID, 'clientid_INT')
+            select = Select(dropdown)  # Create an instance of Select using the dropdown element
+            continue_btn = self.find_element(By.CSS_SELECTOR, "input[type='submit']")
+            # If a dropdown index is specified, select the option at that index, then click continue button
+            if dropdown_index is not None:
+                select.select_by_index(dropdown_index)  # Use the select_by_index method on the Select instance
+                dropdown_name = select.first_selected_option.get_attribute("text")  # Get the name from the selected option
+                continue_btn.click()  # Click the continue button
+            else:
+                select.select_by_index(0)  # Use the select_by_index method on the Select instance
+                dropdown_name = select.first_selected_option.get_attribute("text")  # Get the name from the default option
+                  # Return the name from the dropdown option
+                continue_btn.click()  # Click the continue button
+                
         except WebDriverException:
             current_address = self.current_url
             print(f"Failed to reach the specified address. Current address: {current_address}")
-
             manual_url = input("Failed to reach the specified address. Enter URL manually and press enter")
+            self.get(manual_url)
+        except Exception as e:
+            print(f"An error occurred while selecting the dropdown option: {e}")
+            print(Exception.__traceback__)
+            logging.error(f"An error occurred while selecting the dropdown option: {e}")     
+   
+   
+    def open_browser_to_site(self):
+        # Add your code here
+
+    # Navigate to the desired URL
+    # Specify the path to the Edge driver executable
+    #If there's an element from their login, sign in
+    # If there's an element from their 2fa, wait for action 
+        try:
+
+            if 'depaul.footholdtechnology.com' in self.current_url:
+
+
+            self.get('https://depaul.footholdtechnology.com') 
+            if EC.presence_of_element_located((By.ID, 'handle')):
+                    self.sign_in()
+            else:pass
+                #if 2fa, wait for action
+            if EC.presence_of_element_located((By.ID, 'auth-code')):
+                print("waiting for 2fa")
+                wait(self,60).until_not(EC.presence_of_element_located((By.ID, 'auth-code'))) 
+            self.get('https://depaul.footholdtechnology.com/awards/charts/medical/support_services_contacts')
+            wait(self,45).until(method=EC.presence_of_element_located((By.ID, 'clientid_INT')))
+            JoeDriver.dropdown_select(self)   
+            JoeDriver.dropdown_select(self, 1)
+            
+    
+        except WebDriverException:
+            # If the specified address cannot be reached, get the current address from the open active instance
+            current_address = self.current_url
+            print(f"Failed to reach the specified address. Current address: {current_address}")
+    
+            manual_url = input("Failed to reach the specified address. enter url manually and press enter")
             self.get(manual_url)
         except Exception:
             print(Exception.__traceback__)
-
-    def end_record(self):
-        pass
-
-
-contacts_dict = {}  # checked on the info screen
-check_dict = {}  # checked on the history screen
-        
-
-
+    
+    
 
 
 # Usually don't use globals, but this is a quick and dirty script.
@@ -138,75 +207,21 @@ check_dict = {}  # checked on the history screen
 #driver = webdriver.Edge(service=service, options=options)
 
 def main():
-    driver = JoeDriver()
-    open_browser_to_site(driver)
+    load_dotenv() 
+    driver = JoeDriver(wait)
+    driver.open_browser_to_site()
     driver.sign_in()
+    # go to contact screen
    
 
-def dropdown_select(driver, dropdown_index=None, dropdown_locatorza=None, dropdown_length=None ): 
-#    dropdown_length = len(dropdown.options)
-    #dropdown index would be the iteration number of the loop
-    dropdown = Select(driver.EC.find_element((By.ID, 'clientid_INT')))
-    if dropdown_index is not None:
-        dropdown.select_by_index(dropdown_index)
-        dropdown_name = dropdown.options[dropdown_index].text  # Get the name from the selected option
 
-    else:
-        dropdown.select_by_index(0)
-        dropdown_name = dropdown.options[0].text  # Get the name from the default option
-    return dropdown_name  # Return the name from the dropdown option
-    
-def end_record():
-    pass
-
-def submit_button_click(driver, dropdown_index=None):
-# Duplicate the tab, select the correct person from the drop down, go to new tab, Click the history button in the new duplicated tab, click the continue button,  go back to the other tab,    hit the update button at the bottom of history 
-    
-    first_button = (driver.find_element((By.CSS_SELECTOR, "input[type='submit']")))
-    first_button.click()
-
-def config_driver_default_instance_():
-    pass
     
 
- 
-def compare_data(driver):
-    pass
-def open_browser_to_site(driver):
 
 
-    # Navigate to the desired URL
-    # Specify the path to the Edge driver executable
-    #If there's an element from their login, sign in
-    # If there's an element from their 2fa, wait for action 
-    try:
-        driver.get('https://depaul.footholdtechnology.com') 
-        if EC.presence_of_element_located((By.ID, 'handle')):
-                driver.sign_in()
-            #if 2fa, wait for action
-        if EC.presence_of_element_located((By.ID, 'auth-code')):
-            print("waiting for 2fa")
-            wait(driver,60).until_not(EC.presence_of_element_located((By.ID, 'auth-code'))) 
-        driver.get('https://depaul.footholdtechnology.com/awards/charts/medical/support_services_contacts')
-        wait(driver,45).until(method=EC.presence_of_element_located((By.ID, 'clientid_INT')))
-        dropdown_select(driver)    
-        submit_button_click(driver)
-
-    except WebDriverException:
-        # If the specified address cannot be reached, get the current address from the open active instance
-        current_address = driver.current_url
-        print(f"Failed to reach the specified address. Current address: {current_address}")
-
-        manual_url = input("Failed to reach the specified address. enter url manually and press enter")
-        driver.get(manual_url)
-    except Exception:
-        print(Exception.__traceback__)
-        
-
-
-contacts_dict = {} #checked on the info screen
-check_dict = {} #chwcked on the history screen
-title_list = ['Dentist','Psychiatrist','Physician','Surgery','Therapist']
+info_dict = {} #checked on the info screen
+history_dict = {} #chwcked on the history screen
+role_list = ['Dentist','Psychiatrist','Physician','Surgery','Therapist']
 done_list = []
 dropdown_index= 0
 
@@ -214,18 +229,29 @@ dropdown_index= 0
 
 #end record decision logic 
 # decision
-#if end date, ignore
-#if not on the info screen that matches the title on the right screen, and has a cherck box, and has no end date
-#if 
-# if no end date, and box is available, fill in end date
+
+
 
 # Close the browser window.
-print('Done.')
+
+if __name__ == '__main__':
+    main()
+    print('Done.')
 
 
 
 
+#compare logic
 
+#if end date, ignore
+#if not on the info screen that matches the title on the right screen, 
+#if <i>discontinued</i> in the table
+# if no end date, and box is available, fill in end date
+    #find the role as the key value in the history screen, add the key and value to the dictionary
+    #find the role as the key value in the info screen, add the key and value to the dictionary
+    #compare the two dictionaries:
+        #if they're the same/ fuzzy matches, do nothing
+        #if they're different, add the end date to the info screen on the 
 
 
 
